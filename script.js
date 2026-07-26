1c52f24deb71fb0efbce100b5020972827516ce0
juanoscarpa56-art <juanoscarpa56@gmail.com>
2026-07-25 22:15:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
+const reveals = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("active");
+        }
+    });
+}, {
+    threshold: 0.15
+});
+
+reveals.forEach(el => observer.observe(el));
+
 const themeToggle = document.getElementById("themeToggle");
 
+// Cargar tema guardado
+if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark");
+    themeToggle.textContent = "☀️";
+}
+
+// Cambiar tema
 themeToggle.addEventListener("click", () => {
-    alert("El botón funciona");
+    document.body.classList.toggle("dark");
+
+    if (document.body.classList.contains("dark")) {
+        localStorage.setItem("theme", "dark");
+        themeToggle.textContent = "☀️";
+    } else {
+        localStorage.setItem("theme", "light");
+        themeToggle.textContent = "🌙";
+    }
 });
